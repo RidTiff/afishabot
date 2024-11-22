616208d8ac69fcf4af0067febf0785d246e8a22a
RidTiff <sergey@campanelli.ru>
2024-11-22 22:55:50 +0300

    Add image, fonts and shadow

--- src/Page/App/App.jsx
+++ src/Page/App/App.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import moment from 'moment';
 import './App.css';
 import Card from '../Card/Card.js'
-import dvor from '../../img/dvor.jpg'
 import brod from '../../img/brod.jpg'
-import pastel from '../../img/pastel.jpg'
-import shagal from '../../img/shagal.jpg'
 
 class App extends React.Component {
   constructor(props) {
@@ -19,7 +16,7 @@ class App extends React.Component {
       dataOfEvent: [
         {
           id: 0,
-          image: dvor, 
+          image: brod, 
           name: 'Дворянское воспитание. Обычаи и традиции',
           expo: {name:'Остафьево', address: "Лаврушинский пер., 10"},
           begin: "2024-05-31",
@@ -46,8 +43,7 @@ class App extends React.Component {
 
   handlerCardClick(e) {
       this.setState( prevState => {
-        this.selectedCard = prevState.dataOfEvent[e.target.parentElement.id]
-        console.log(e.target.parentElement.id)
+        window.scrollTo(0, 0)
         return{
           ...prevState,
           toggleCard: !prevState.toggleCard,
@@ -62,13 +58,13 @@ class App extends React.Component {
     <main className={"main" + (!this.state.toggleCard ? ' list': ' toggleCard')}>
       { !this.state.toggleCard ? this.state.dataOfEvent.map((item) => 
         (
-          <Card key={item.id} id={this.state.dataOfEvent.indexOf(item)} img={brod} name={item.name} place={item.expo.name + '. Адрес - ' + item.expo.address} date={moment(item.begin).diff(moment(), 'days') > 0 ? 'Идёт с ' + item.begin + ', до ' + item.end : (moment(item.end).diff(moment(), 'days') > 0 ? 'Идёт сейчас, до ' + item.end : 'Сейчас уже закрылась :(')} src={item.src} onClick={this.handlerCardClick}/>
+          <Card key={item.id} id={this.state.dataOfEvent.indexOf(item)} img={item.image} name={item.name} place={item.expo.name + '. Адрес - ' + item.expo.address} date={moment(item.begin).diff(moment(), 'days') > 0 ? 'Идёт с ' + item.begin + ', до ' + item.end : (moment(item.end).diff(moment(), 'days') > 0 ? 'Идёт сейчас, до ' + item.end : 'Сейчас уже закрылась :(')} src={item.src} onClick={this.handlerCardClick}/>
         )) : 
-        <>
+        <div className='container'>
           <h3 className='name'>
             {this.state.selectedCard.name}
           </h3>
-          <img src={brod} className='img'/>
+          <img src={this.state.selectedCard.image} className='img'/>
           <p className='date'>
             {moment(this.state.selectedCard.begin).diff(moment(), 'days') > 0 ? 'Идёт с ' + this.state.selectedCard.begin + ', до ' + this.state.selectedCard.end : (moment(this.state.selectedCard.end).diff(moment(), 'days') > 0 ? 'Идёт сейчас, до ' + this.state.selectedCard.end : 'Сейчас уже закрылась :(')}
           </p>
@@ -79,7 +75,7 @@ class App extends React.Component {
             {this.state.selectedCard.description}
           </p>
           <button className='buttonBack' onClick={this.handlerCardClick}>Назад</button>
-        </>
+        </div>
       }
     </main>
     );
